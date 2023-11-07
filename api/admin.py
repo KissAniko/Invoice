855e0a712ff7cae45f66f3cdf68ccd2ade81acb7
KissAniko <126045521+KissAniko@users.noreply.github.com>
2023-11-07 18:38:09 +0100

    ora vege

---
+++ api/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Invoice, User
+
+admin.site.register(Invoice)
+admin.site.register(User)
+
+# Register your models here.
---
+++ api/serializers.py
@@ -0,0 +1,13 @@
+from .models import User, Invoice
+from rest_framework import serializers
+
+class InvoiceSerializer(serializers.ModelSerializer):
+ class Meta:
+  model = Invoice
+  fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+
+ class Meta:
+  model = User
+  fields = '__all__'
